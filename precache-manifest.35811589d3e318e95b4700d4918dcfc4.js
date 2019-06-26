8b44fbe50023ad3404ca7436aec93842bd126c28
Marcin Warpechowski <warpech@gmail.com>
2019-06-26 11:21:28 +0200

    Updates

---
+++ precache-manifest.35811589d3e318e95b4700d4918dcfc4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca69c2792a844f78bc33f99809f5551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5cb41b47886e8d98e63",
+    "url": "/static/css/main.4d512f42.chunk.css"
+  },
+  {
+    "revision": "cd91b22b9efe3c28877c",
+    "url": "/static/js/2.b9c82188.chunk.js"
+  },
+  {
+    "revision": "b5cb41b47886e8d98e63",
+    "url": "/static/js/main.506ed9fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ce422b141b934425cac12756a48f7e23",
+    "url": "/static/media/logo.ce422b14.png"
+  },
+  {
+    "revision": "84f76d97490b24b0fdc551e5d9f5e2e1",
+    "url": "/static/media/polymer.84f76d97.png"
+  },
+  {
+    "revision": "fe7d34fea2699c98a40935d34deeec9b",
+    "url": "/static/media/react.fe7d34fe.png"
+  },
+  {
+    "revision": "acd3acb168892d3591bda297d8f2ed86",
+    "url": "/static/media/vue.acd3acb1.png"
+  }
+]);
