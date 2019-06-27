b6897ecda09d12fc2849c327655c41d0bdd4c526
Marcin Warpechowski <warpech@gmail.com>
2019-06-27 12:38:37 +0200

    change Polymer and Vue demos to be OT
    remove separate "Polymer OT" demo

--- public/lab/mock-server/src/mock-server.js
+++ public/lab/mock-server/src/mock-server.js
@@ -1,6 +1,11 @@
 const fetchMock = require('fetch-mock');
 
+const localVersionPath = "/_ver#s";
+const remoteVersionPath = "/_ver#c$";
+
 const obj = {
+    "_ver#s": 0,
+    "_ver#c$": 0,
     user: {
       fullName: "",
       firstName$: "",
@@ -9,10 +14,18 @@ const obj = {
     }
 };
 
-function generateReplaceOperation(obj, ...paths) {
+function generateVersionOperations() {
+    obj["_ver#s"] += 1;
+    return [
+        {op: "replace", path: localVersionPath, value: obj["_ver#s"]},
+        {op: "test", path: remoteVersionPath, value: obj["_ver#c$"]}
+    ];
+}
+
+function generateReplaceOperation(tree, ...paths) {
     return paths.map(path => {
         const prop = path.substring(path.lastIndexOf('/') + 1);
-        return {op: "replace", path: path, value: obj[prop]}
+        return {op: "replace", path: path, value: tree[prop]}
     });
 }
 
@@ -33,37 +46,48 @@ fetchMock.mock('*', (url, req) => {
     Palindrom library has an option to run as a server in NodeJS, providing you with such functionality. However, it is not presented in this demo.
     */
     if (req.headers.Accept === 'application/json-patch+json') {
-        let patch;
-        let incomingPatch = req.body ? JSON.parse(req.body) : [];
-        if (incomingPatch.length && incomingPatch[0].op === 'replace') {
-            if (incomingPatch[0].path === '/user/firstName$') {
-                setPerson(incomingPatch[0].value, null);
-                patch = generateReplaceOperation(obj.user, '/user/fullName');
+        let patch = [];
+        const incomingPatch = req.body ? JSON.parse(req.body) : [];
+        if (incomingPatch.length) {
+            if (incomingPatch[1].op !== 'test') {
+                throw new Error("The client does not seem to implement OT");
             }
-            else if (incomingPatch[0].path === '/user/lastName$') {
-                setPerson(null, incomingPatch[0].value);
-                patch = generateReplaceOperation(obj.user, '/user/fullName');
-            }
-            else if (incomingPatch[0].path === '/user/resetNameClicked$' && (incomingPatch[0].value === "true" || incomingPatch[0].value === true)) {
-                // Polymer sends string "true", because the value is bound to a HTML attribute
-                // React sends boolean true
-                setPerson("Isaac", "Newton");
-                patch = generateReplaceOperation(obj.user, '/user/resetNameClicked$', '/user/firstName$', '/user/lastName$', '/user/fullName');
-            }
-            else {
-                console.log("Unrecognized patch", incomingPatch);
-                throw new Error("Unrecognized patch");
-            }
+            incomingPatch.forEach(operation => {
+                if (operation.op === 'replace') {
+                    if (operation.path === remoteVersionPath) {
+                        obj["_ver#c$"] = operation.value;
+                    }
+                    else if (operation.path === '/user/firstName$') {
+                        setPerson(operation.value, null);
+                        patch.push(...generateReplaceOperation(obj.user, '/user/fullName'));
+                    }
+                    else if (operation.path === '/user/lastName$') {
+                        setPerson(null, operation.value);
+                        patch.push(...generateReplaceOperation(obj.user, '/user/fullName'));
+                    }
+                    else if (operation.path === '/user/resetNameClicked$' && (operation.value === "true" || operation.value === true)) {
+                        // Polymer sends string "true", because the value is bound to a HTML attribute
+                        // React sends boolean true
+                        setPerson("Isaac", "Newton");
+                        patch.push(...generateReplaceOperation(obj.user, '/user/resetNameClicked$', '/user/firstName$', '/user/lastName$', '/user/fullName'));
+                    }
+                    else {
+                        console.log("Unrecognized patch", operation);
+                        throw new Error("Unrecognized patch");
+                    }
+                };
+            });
         }
         else if (url.endsWith('/subpage.html')) {
             setPerson("Nikola", "Tesla");
-            patch = generateReplaceOperation(obj.user, '/user/firstName$', '/user/lastName$', '/user/fullName');
+            patch.push(...generateReplaceOperation(obj.user, '/user/firstName$', '/user/lastName$', '/user/fullName'));
 
         }
         else {
             setPerson("Albert", "Einstein");
-            patch = generateReplaceOperation(obj.user, '/user/firstName$', '/user/lastName$', '/user/fullName');
+            patch.push(...generateReplaceOperation(obj.user, '/user/firstName$', '/user/lastName$', '/user/fullName'));
         }
+        patch.unshift(...generateVersionOperations());
         return {
             status: 200,
             headers: { contentType: 'application/json-patch+json' },
