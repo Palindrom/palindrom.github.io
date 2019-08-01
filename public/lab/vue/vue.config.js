a480162a21406c1dcd2e7c437d37075771b4d455
Marcin Warpechowski <warpech@gmail.com>
2019-08-01 09:56:59 +0200

    turn off ESLint, otherwise it throws errors "No ESLint configuration found." for dependencies loaded using ../ or npm link

--- public/lab/vue/vue.config.js
+++ public/lab/vue/vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: './' // use relative URLs in generated code. This is needed because the app is deployed in a deep path at GitHub Pages
+    publicPath: './', // use relative URLs in generated code. This is needed because the app is deployed in a deep path at GitHub Pages,
+    lintOnSave: false // turn off ESLint, otherwise it throws errors "No ESLint configuration found." for dependencies loaded using ../ or npm link
 };
